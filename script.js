6f20a05537dfc62d28dc2a698df412c67862c1a1
unknown <charlyyoucan225@gmail.com>
2023-10-24 02:15:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// ---------------typed js-----------
+
+const typed = new Typed(".bodyA h2", {
+  strings: [
+    "CONSULTATION MEDECALE ET TRAITEMENT.",
+    "CARDIOGRAPHIE.",
+    "TRIAGE ET INTERVENTION POST-ACCIDENT.",
+    "INTERVENTION ET SOIN .",
+    "COORDINATION DES SOINS ET COACHING SANTE",
+    "GESTION MEDICO-ADMINISTRATIVE.",
+  ],
+  typeSpeed: 50,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
